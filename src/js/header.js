bedeef00738753554d489f8b81b47777d57d2741
Asya Yeromina <asyayeromina@gmail.com>
2025-02-10 15:24:23 +0100

    add header modal for small devices (burger btn + vertical modal)

---
+++ src/js/header.js
@@ -0,0 +1,24 @@
+var body = document.querySelector(".body");
+var header = document.querySelector(".header");
+var burgerButton = document.querySelector(".header-burger-button");
+var overlay = document.createElement("div");
+overlay.classList.add("overlay");
+document.body.appendChild(overlay);
+
+if (body !== null && header !== null && burgerButton !== null) {
+
+  if (!body.classList.contains("body--header-collapsed")) {
+    body.classList.add("body--header-modal");
+  }
+
+  burgerButton.addEventListener("click", function () {
+    body.classList.toggle("body--header-collapsed");
+    body.classList.add("body--header-modal");
+
+    if (body.classList.contains("body--header-collapsed")) {
+      body.classList.remove("body--header-modal");
+    }
+  });
+}
+
+// Body on other pages (not index) should has "body" class
